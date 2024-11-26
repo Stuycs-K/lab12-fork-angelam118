309877a94efeda9a67e8952a86e328e62c4bfd9b
Angela Mashuryan <m.angela.0118@gmail.com>
2024-11-26 15:30:48 -0500

    in class 2

--- main.c
+++ main.c
@@ -2,10 +2,28 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
 int main(){
     printf("%d about to create 2 child processes.\n", getpid());
-    
+    pid_t p;
+    p = fork();
+    if(p<0){
+        perror("fork fail");
+        exit(1);
+    }
+    else if (p==0){
+        int random_number = open("/dev/random/", O_RDONLY);
+        if (random_number == -1){
+            printf("errno %d\n",errno);
+            printf("%s\n",strerror(errno));
+            exit(1);
+        }
+        
+
+    }
 
 }
