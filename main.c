ce6aae744908340ab2c59377eb251867859622da
Angela Mashuryan <m.angela.0118@gmail.com>
2024-11-27 01:13:12 -0500

    first child

--- main.c
+++ main.c
@@ -9,19 +9,32 @@
 
 int main(){
     printf("%d about to create 2 child processes.\n", getpid());
-    pid_t p;
-    p = fork();
-    if(p<0){
+    pid_t p1, p2;
+    p1 = fork();
+    if(p1<0){
         perror("fork fail");
         exit(1);
     }
-    else if (p==0){
+    else if (p1==0){
         int random_number = open("/dev/random/", O_RDONLY);
         if (random_number == -1){
             printf("errno %d\n",errno);
             printf("%s\n",strerror(errno));
             exit(1);
         }
+        unsigned char one_byte;
+        read(random_number, &one_byte, 1);
+        close(random_number);
+
+        int random_seconds = (one_byte%5) + 1;
+        printf("%d %d sec\n", getpid(), random_seconds);
+        sleep(random_seconds);
+        printf("%d finished after %d seconds\n", getpid(), random_seconds);
+        exit(random_seconds);
+    }
+    p2 = fork();
+    
+
         
 
     }
